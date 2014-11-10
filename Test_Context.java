17faa2325d9d61e984e6fff785afe4f32c0036db
Ben Tupper <btupper@bigelow.org>
2014-11-10 13:03:50 -0500

    updated tests for Context and Spreadsheet

--- Test_Context.java
+++ Test_Context.java
@@ -5,13 +5,15 @@ import java.awt.*;
 import ij.plugin.*;
 import ij.plugin.frame.*;
 
-public class Test_IniFile implements PlugIn {
+public class Test_Context implements PlugIn {
 
 	public void run(String arg) {
 
 		try {
-			IniFile cfg = new IniFile("/Users/Shared/data/macf/NASAcarbon/fcam/242-142907/242-142907.ctx");
+			Context cfg = new Context("/Users/Shared/data/fac/NASAcarbon/fcam/242-142907/242-142907.ctx");
 			IJ.log("[General] RunStartTime="+ cfg.getString("General", "RunStartTime"));
+			IJ.log("[General] TechnicianName="+ cfg.getString("General", "TechnicianName"));
+			IJ.log("[Fluid] CalibrationConstant="+ cfg.getDouble("Fluid", "CalibrationConstant"));
 		} catch (Exception e) {
          		IJ.log("Caught Exception: " + e.getMessage());
       	}
---
+++ Test_Spreadsheet.java
@@ -0,0 +1,22 @@
+import ij.*;
+import ij.process.*;
+import ij.gui.*;
+import java.awt.*;
+import ij.plugin.*;
+import ij.plugin.frame.*;
+
+public class Test_Spreadsheet implements PlugIn {
+
+	public void run(String arg) {
+
+		try {
+			Spreadsheet x = new Spreadsheet("/Users/Shared/data/fac/NASAcarbon/fcam/242-142907/242-142907.csv", ",");
+         IJ.log("size = " + x.size());
+         IJ.log("ncol = " + x.countColumns() );
+         IJ.log("sigma_intensity [2] =" + x.getDataElementDouble("sigma_intensity", 2));
+		} catch (Exception e) {
+         		IJ.log("Caught Exception: " + e.getMessage());
+      	}
+	}
+
+}
