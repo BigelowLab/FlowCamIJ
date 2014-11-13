71a0e70266b047a2e4fe774fb9fc29767aebb398
Ben Tupper <btupper@bigelow.org>
2014-11-13 13:08:10 -0500

    prettify yesterdays work storm

--- FlowCam.java
+++ FlowCam.java
@@ -21,13 +21,13 @@ import ij.measure.Calibration;
 
 public class FlowCam {
    
-   public Context ctx;
-   public Spreadsheet data;
-   
    public File inputDir;
    public String name;
    public File outputDir;
-    
+
+   public Context ctx;
+   public Spreadsheet data;
+   
    public String[] collageFilenames;
    public String[] collageMaskFilenames;
    public String[] calFilenames;
@@ -79,8 +79,8 @@ public class FlowCam {
          }
          data = new Spreadsheet(inputDir.getPath() + File.separator + this.name + ".csv",",");
 
-         populate_file_lists();
-         cal = new Calibration();
+         populateFileLists();
+         setCalibration();
          
       } else {
          IJ.log("path not found: " + inputDir.getPath());
@@ -88,14 +88,38 @@ public class FlowCam {
       }
    
       return ok;
-   }
+   } //init
    
+   /**
+      Set the calibration (pixel size) for all loaded FlowCam images
+      @param pixelWidth the pixel size in horizontal
+      @param pixelHeight the pixel size in vertical
+      @param unit the name of the unit (typically "um")
+   */
+   public void setCalibration(double pixelHeight, double pixelWidth, String unit){
+      
+      if (cal == null) cal = new Calibration()
+      cal.pixelWidth = pixelWidth;
+      cal.pixelHeight = pixelHeight;
+      cal.setUnit(unit);
+      if (rawImages != null) rawImages.setCalibration(cal.copy());
+      if (calImages != null) calImages.setCalibration(cal.copy());
+      if (collageImages != null) collageImages.setCalibration(cal.copy());
+      if (collageMaskImages != null) collageMaskImages.setCalibration(cal.copy());
+   } //setCalibration
    
+   /*
+    Set the calibration for all images based upon values in the Context file
+   */
+   public void setCalibration(){
+      double pixelSize = cal.getDouble("Fluid", "CalibrationConstant", 1.0);
+      setCalibration(pixelSize, pixelSize, "um"); 
+   }//setCalibration
    
    /**
       Load (but don't show) the image stack specified.  We don't auto load these for
-      two reasons.  One is that the Calibration may need to be set and Two is
-      there may be no need for raw of cal images.
+      two reasons: (a) the Calibration may need to be set and (b) 
+      there may be no need to load all - it depends upon the analysis.
       @param name the name of the image stack ["calImages","rawImages", "collageImages", "collageMaskImages", "all"]
    */
    public void loadStack(String iname){   
@@ -117,17 +141,21 @@ public class FlowCam {
             loadStack(inames[i]);
          }
       }
-   }
+   } //loadStack
    
    /**
-      Populate the file lists - self-explanatory?
+      Populate the file lists - self-explanatory? - for image files.  These are
+      preloaded into calFilenames, rawFilenames, collageMaskFilenames and collageFilenames.
+      We assume that the weight of preloading these is fairly light.  Be aware that
+      if any are missing from the input directory then the string arrays may have
+      zero length.
    */
-   public void populate_file_lists(){
+   public void populateFileLists(){
      calFilenames = matchFiles(inputDir,"calImages"); 
      rawFilenames = matchFiles(inputDir, "rawImages"); 
      collageMaskFilenames = matchFiles(inputDir, "collageMaskImages"); 
      collageFilenames = getUnique(data.getDataColumn("collage_file"));
-   }
+   } //poulateFileLists
    
    /*
       Match files given the specified regex pattern
@@ -162,12 +190,12 @@ public class FlowCam {
            }
          });      
       } else if (what.equalsIgnoreCase("collageImages")){
-         //these are easier to lift from the data file
+         //these are easiest to lift from the data file
          files = getUnique(data.getDataColumn("collage_file"));
       }
       
       return files;
-   }
+   } //matchFiles
    
    /**
       Return a strng array of the unique elements in a string array
@@ -180,7 +208,7 @@ public class FlowCam {
       unique = set.toArray( new String[0]);
       Arrays.sort(unique);
       return unique;
-   }
+   } //getUnique
    
 
 } // class FlowCam
